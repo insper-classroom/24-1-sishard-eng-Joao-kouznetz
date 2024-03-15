729479b51ba549915d072d6aa634a3951b6d787b
Joao <joao.bresserpereira@gmail.com>
2024-03-14 23:02:33 -0300

    EX1

--- atv/04-condicionais-funcoes/solucao_ex1.c
+++ atv/04-condicionais-funcoes/solucao_ex1.c
@@ -1,3 +1,11 @@
-/* Implementar função solucao_ex1 */
 
+#include <stdbool.h>
 
+bool function(long rdi, long rsi, long rdx) {
+    long rax = rdi + rsi;             // lea    (%rdi,%rsi,1),%rax
+    long rcx = rax + (rdx * 4);       // lea    (%rax,%rdx,4),%rcx
+    rdi *= rdi;                       // imul   %rdi,%rdi
+    rax = rdi + (rsi * 2);            // lea    (%rdi,%rsi,2),%rax
+    rdx += rax;                       // add    %rax,%rdx
+    return rdx >= rcx;                // cmp    %rdx,%rcx; setge  %al; movzbl %al,%eax
+}
