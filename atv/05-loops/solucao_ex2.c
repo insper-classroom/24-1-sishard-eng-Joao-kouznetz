7a4906a983803bb21c2c30e9ca2dfae054aa0315
Joao <joao.bresserpereira@gmail.com>
2024-03-17 19:38:21 -0300

    atv5

--- atv/05-loops/solucao_ex2.c
+++ atv/05-loops/solucao_ex2.c
@@ -3,3 +3,19 @@
  *
  * Chame sua função de soma_n_solucao */
 
+long soma_n_solucao(int edi) {
+    long eax = 0x0;
+    long edx = 0x0;
+    
+    while_goto:
+    if (eax >= edi) {
+        return edx;
+    }
+    
+    long rcx = (long)eax;
+    edx += rcx;
+    
+    eax += 0x1;
+    goto while_goto;
+}
+
--- atv/05-loops/solucao_ex3.c
+++ atv/05-loops/solucao_ex3.c
@@ -2,3 +2,28 @@
  * presente no arquivo ex3.o
  * 
  * Chame sua função de ex3_solucao */
+long ex3_solucao(long rdi, long rsi) {
+    long rcx = 0x0;
+    long r8d = 0x0;
+    
+    while_goto:
+    if (rcx >= rdi) {
+        return r8d;
+    }
+    
+    long rax = rcx;
+    long rdx = 0x0;
+
+    // Divisão de rax por rsi (rax / rsi)
+    rdx = rax % rsi;
+
+    if (rdx != 0x0) {
+        goto loop_continue;
+    }
+    
+    r8d += rcx;
+    
+    loop_continue:
+    rcx += 0x1;
+    goto while_goto;
+}
