7365a1d9762d92fe976162c89f1cf2d4dfdfb9e9
Joao <joao.bresserpereira@gmail.com>
2024-03-22 23:07:04 -0300

    atv6 correta

--- atv/06-variaveis-locais/solucao_ex4.c
+++ atv/06-variaveis-locais/solucao_ex4.c
@@ -1,7 +1,17 @@
-int ex4_solucao(int *rdi, int *rsi, int *rdx, int ebx) {
-    printf(rdi, "rdi");
-    printf(rsi);
-    int eax = 0;
-    eax = *rsi + *rdx * 2 + ebx;
-    return eax;
+// int ex4_solucao(int *rdi, int *rsi, int *rdx, int ebx) {
+//     printf(rdi, "rdi");
+//     printf(rsi);
+//     int eax = 0;
+//     eax = *rsi + *rdx * 2 + ebx;
+//     return eax;
+// }
+int ex4_solucao(int rdi)
+
+{
+  int rdx;
+  int rsi;
+  
+  scanf("%d %d",&rsi,&rdx);
+  return rsi + rdx * 2 + rdi;
 }
+
